d46e2712f6e22b77656e979929027819f22226f1
Ricardo Alcantara <alcantararicardo04@gmail.com>
2024-02-11 20:09:13 -0500

    authControllers: metodo post para signup creado con token

--- api/src/controllers/authControllers.js
+++ api/src/controllers/authControllers.js
@@ -1,14 +1,32 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
 
- 
-// const User = require('../models/userModel');
-// const verifyToken = require('./verifyToken');
+const User = require("../models/userModel");
+const verifyToken = require("./verifyToken");
 
-// const jwt = requite('jsonwebtoken');
-// const config = require('../conf');
+const jwt = requite("jsonwebtoken");
+const config = require("../conf");
 
-// router.post('/signup',async(req,res)=>{
+router.post("/signup", async (req, res) => {
+try {
+    const {username,email,password} = req.body;
+    const user = new User({
+        username,
+        email,
+        password
+    });
+    user.password = await user.encryptPassword(password);
+    await user.save();
+    const token = jwt.sign({id:user.id}, conf.secret,{
+        expiresIn: '24h'
+    });
+    res.status(200).json({auth:true,token});
+    
+} catch (error) {
+    console.log(error)
+    res.status(500).send('There was a problem signin');
+}
 
-// })
+})
+
  
